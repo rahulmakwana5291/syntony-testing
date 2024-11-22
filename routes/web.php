5a8163c0bea7044b54b473fa139056182e4b6b53
Rahul Makwana <rahul.makwana@codefylabs.com>
2024-11-22 21:17:41 +0530

    fix route

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 
 use App\Http\Controllers\DynamoDbController;
 
-Route::get('/dynamodb/user-settings', [DynamoDbController::class, 'getAllUserSettings']);
+Route::get('/', [DynamoDbController::class, 'getAllUserSettings']);
 Route::get('/dynamodb/item', [DynamoDbController::class, 'getItem']);
 Route::get('/dynamodb/query', [DynamoDbController::class, 'queryTable']);
